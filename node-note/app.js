6d05ee6e95420fe640240cecff23f0107615fba2
jin <jinyuli91@gmail.com>
2019-01-02 15:34:54 -0500

    learned to use lodash ulities

--- node-note/app.js
+++ node-note/app.js
@@ -3,9 +3,16 @@ console.log('starting app')
 const fs = require('fs');
 const os = require('os');
 const note = require('./note.js')
+const _ = require('lodash')
 
 // let user = os.userInfo();
 // fs.appendFileSync('greeting.txt', `Hello ${user.username}! You are ${note.age}.`);
+//
+// let res = note.add(3, 7);
+// console.log(res)
 
-let res = note.add(3, 7);
-console.log(res)
+console.log(_.isString(true));
+console.log(_.isString('true'));
+
+var filterArray = _.uniq(['Andrew', 'Andrew', '1', '1', '2', '2', '4'])
+console.log(filterArray)
