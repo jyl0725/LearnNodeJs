66d537292eea298720dcda0921ad95c624b610ad
jin <jinyuli91@gmail.com>
2019-01-04 16:21:45 -0500

    learned writefilesync and readfilesync

---
+++ node-note/playground/json.js
@@ -0,0 +1,30 @@
+// let obj = {
+//   name: 'Andrew'
+// };
+//
+// let stringObj = JSON.stringify(obj);
+//
+// console.log(typeof stringObj)
+// console.log(stringObj)
+
+// let personString = '{"name": "Andrew", "age" : 25}';
+// var person = JSON.parse(personString);
+// console.log(typeof person)
+// console.log(person)
+
+const fs = require('fs');
+
+let originalNote = {
+  title: 'some title',
+  body: 'some body',
+}
+
+let originalNoteObj = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteObj);
+
+let noteString = fs.readFileSync('notes.json')
+
+let note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title)
