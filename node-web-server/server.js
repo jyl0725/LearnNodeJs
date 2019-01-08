fd5411fc783aaaf9ea49014adc340810c9280862
jin <jinyuli91@gmail.com>
2019-01-08 17:42:02 -0500

    learned handlebar parital and helper

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,21 +1,33 @@
 const express = require('express')
+const hbs = require('hbs')
 
 let app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>')
-  res.send({
-    name: 'Jin',
-    likes: [
-      'basketball',
-      'travels'
-    ]
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome Here',
   })
-});
+})
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
-})
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
 
 app.get('/bad', (req, res) => {
   res.send({
