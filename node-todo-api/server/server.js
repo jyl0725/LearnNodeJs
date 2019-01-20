a3e583022db70974bcb5984e10e32d046d95036d
jin <jinyuli91@gmail.com>
2019-01-19 20:21:46 -0500

    build patch request

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/Todo');
@@ -44,6 +45,47 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send()
   });
 });
+
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id)
+  .then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    return res.send({todo})
+  }).catch( err => res.status(400).send(err))
+})
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+  .then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo})
+  }).catch(e => res.status(400).send())
+})
 
 app.listen(port, () => {
   console.log(`started up at port ${port}`)
--- node-todo-api/server/test/server.test.js
+++ node-todo-api/server/test/server.test.js
@@ -100,3 +100,38 @@ describe('GET /Todos/:id', () => {
       .end(done)
   });
 });
+
+describe('Delete /Todos:id', () => {
+  it('should remove find and remove one todo', (done) => {
+    request(app)
+      .delete(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect( res => {
+        expect(res.body.todo._id).toBe(todos[0]._id.toHexString())
+      })
+      .end((err, res) =>{
+        if (err) {
+          return done(err)
+        }
+        Todo.findById(todos[0]._id.toHexString())
+        .then((todo) => {
+          expect(todo).toBeFalsy();
+          done();
+        }).catch((e) => done(e))
+      })
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done)
+  })
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done)
+  })
+});
